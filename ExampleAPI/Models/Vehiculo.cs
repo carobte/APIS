7c9fdc7232be7780bf689b67cbf1abeb96b7eace
carobte <caro.bustamante.escobar@gmail.com>
2024-09-02 07:25:54 -0500

    feat: Conexión entre modelos

--- ExampleAPI/Models/Vehiculo.cs
+++ ExampleAPI/Models/Vehiculo.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,19 @@ namespace ExampleAPI.Models
 {
     public class Vehiculo
     {
-        public int Id {get;set;}
-        public required string Marca {get;set;}
-        public required string Modelo {get;set;}
-        public required int Año {get;set;}
-        public required string Color {get;set;}
-        public required string Tipo { get;set;}
+        [Key]
+        public int Id { get; set; }
+        public required string Marca { get; set; }
+        public required string Modelo { get; set; }
+        public required int Año { get; set; }
+        public required string Color { get; set; }
+        public required string Tipo { get; set; }
+        public required int PropietarioId { get; set; } // Referencia a la FK
+
+        // Enlaces FK 
+        [ForeignKey("PropietarioId")] // Conexión 
+        public Propietario? Propietario { get; set; }
+
 
     }
 }
