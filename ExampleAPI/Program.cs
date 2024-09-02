32df6a1b5dcab2c200174f9b6f3d16baff4c6030
carobte <caro.bustamante.escobar@gmail.com>
2024-09-02 07:01:10 -0500

    feat: API project created

---
+++ ExampleAPI/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseWelcomePage(); // Welcome page, doesn't show the error message, so we know the API is working
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
